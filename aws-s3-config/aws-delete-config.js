4e598414652d91f3e45ca8fdd9a278abf507aabe
RobinUrcun <107709383+RobinUrcun@users.noreply.github.com>
2024-05-02 14:06:37 +0200

    Route Delete singleImage et singleProduct  ==> OK

--- aws-s3-config/aws-delete-config.js
+++ aws-s3-config/aws-delete-config.js
@@ -1,10 +1,28 @@
 const { DeleteObjectCommand, S3Client } = require("@aws-sdk/client-s3");
 
-exports.awsDeleteConfig = async (file) => {
+exports.awsDeleteConfig = async (mainFile, file) => {
+  console.log(mainFile);
   const client = new S3Client({});
+  if (mainFile) {
+    for (let i = 0; i < mainFile.length; i++) {
+      const mainFileParams = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: `upload/${mainFile[i]}`,
+      };
 
-  const params = { Bucket: process.env.AWS_BUCKET_NAME, Key: `upload/${file}` };
+      await client.send(new DeleteObjectCommand(mainFileParams));
+    }
+  }
+  if (file) {
+    console.log(file.length);
+    for (let i = 0; i < file.length; i++) {
+      const fileParams = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: `upload/${file[i]}`,
+      };
 
-  await client.send(new DeleteObjectCommand(params));
-  return "supprimé";
+      await client.send(new DeleteObjectCommand(fileParams));
+    }
+    return "supprimé";
+  }
 };
