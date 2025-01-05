d6b35dd4944e3088a47793b854ff30d09af17146
RobinUrcun <107709383+RobinUrcun@users.noreply.github.com>
2025-01-05 22:15:14 +0100

    set domain

--- controllers/users.js
+++ controllers/users.js
@@ -127,7 +127,7 @@ exports.logIn = (req, res, next) => {
                 sameSite: "None",
                 maxAge: 3600 * 24,
                 path: "/",
-                domain: "lithosphere.vercel.app",
+                domain: ".",
               })
             );
             return res.status(200).json({
