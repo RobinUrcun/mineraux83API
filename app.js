1fad5046516b109bb243cac7be253a3f73be333d
RobinUrcun <107709383+RobinUrcun@users.noreply.github.com>
2025-01-05 21:50:27 +0100

    url CORS

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://lithosphere-api.vercel.app"
+    "https://lithosphere.vercel.app"
   );
   res.setHeader(
     "Access-Control-Allow-Headers",
